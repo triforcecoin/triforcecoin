5c10efb0d8f0d8e0259cc9b699b59b82fdb21365
Triforcecoin <coins@triforcecoin.com>
2019-01-22 21:18:58 -0600

    use csv

---
+++ precache-manifest.07fd3d28041fd65175d99713b8749138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e065fbb0ebc37b7fefc5",
+    "url": "/static/js/main.e065fbb0.chunk.js"
+  },
+  {
+    "revision": "f046177e150427d66dcf",
+    "url": "/static/js/1.f046177e.chunk.js"
+  },
+  {
+    "revision": "e065fbb0ebc37b7fefc5",
+    "url": "/static/css/main.14d81f3d.chunk.css"
+  },
+  {
+    "revision": "f046177e150427d66dcf",
+    "url": "/static/css/1.bbf2fb1d.chunk.css"
+  },
+  {
+    "revision": "dbedf7dde6efcce8b69a0fe09b70f930",
+    "url": "/index.html"
+  }
+];
