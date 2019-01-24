627cb28bf67e56ac5f63e2d0e2b76561fea04ff4
Triforcecoin <coins@triforcecoin.com>
2019-01-24 13:35:51 -0600

    asic css fix

---
+++ precache-manifest.75d0c05e3987f8480c6ab1e0338766e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f46e899fe49c7ef2cdce",
+    "url": "/static/js/main.f46e899f.chunk.js"
+  },
+  {
+    "revision": "c7024efccf2fb92b65f1",
+    "url": "/static/js/1.c7024efc.chunk.js"
+  },
+  {
+    "revision": "f46e899fe49c7ef2cdce",
+    "url": "/static/css/main.c3f46130.chunk.css"
+  },
+  {
+    "revision": "c7024efccf2fb92b65f1",
+    "url": "/static/css/1.c2d974e2.chunk.css"
+  },
+  {
+    "revision": "53b3a203efc479bc52fd65de326d5658",
+    "url": "/index.html"
+  }
+];
