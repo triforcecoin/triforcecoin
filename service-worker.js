744d2bef4e47c8152218caeed1cf20b2928a6626
Triforcecoin <coins@triforcecoin.com>
2019-01-29 17:21:17 -0600

    fix recent sort

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.de55c00fa7e9f59540b34496a319ce8c.js"
+  "/precache-manifest.05fefd7b5407a89aa781f855799a258a.js"
 );
 
 workbox.clientsClaim();
